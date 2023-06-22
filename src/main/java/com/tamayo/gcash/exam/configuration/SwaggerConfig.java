c72c7e61d2ba2a278c406efa2d9c4cbaf07b33a0
fvaTamayo <137138150+fvaTamayo@users.noreply.github.com>
2023-06-22 14:14:08 +0800

    Update SwaggerConfig.java

--- src/main/java/com/tamayo/gcash/exam/configuration/SwaggerConfig.java
+++ src/main/java/com/tamayo/gcash/exam/configuration/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
             .info(new Info().title(applicationName)
-                .description("Rest API for " + applicationName + " || Source Code: " + "https://github.com/fvaTamayo/deliveryCost")
+                .description("Rest API for " + applicationName + " || Source Code: " + "https://github.com/fvaTamayo/DeliveryCostAPI")
                 .contact(new Contact().name("Faustine Tamayo").email("faustine.tamayo@gmail.com"))
             );
     }
